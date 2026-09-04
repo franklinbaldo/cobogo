808bd1c4ff6a46db756d3ab21eeb02d639b2a27b
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2026-09-04 18:46:17 -0400

    refactor(api): remove Grid and Column component exports

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,6 @@ export { default as Tabs } from './components/Tabs.svelte';
 export { default as TabList } from './components/TabList.svelte';
 export { default as Tab } from './components/Tab.svelte';
 export { default as TabPanel } from './components/TabPanel.svelte';
-export { default as Grid } from './components/Grid.svelte';
-export { default as Column } from './components/Column.svelte';
 export { default as Tooltip } from './components/Tooltip.svelte';
 export { default as Combobox } from './components/Combobox.svelte';
 export { default as DateRangePicker } from './components/DateRangePicker.svelte';
